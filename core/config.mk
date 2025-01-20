e1357f56791a8d7f9bee0490b79bdf65163c3ac8
Rve27 <rve27github@gmail.com>
2025-01-20 07:37:54 +0700

    build: RvOS
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -454,8 +454,8 @@ endif
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(CUSTOM_BUILD),)
-include vendor/aosp/config/BoardConfigCustom.mk
+ifneq ($(RVOS_BUILD),)
+include vendor/rvos/config/BoardConfigCustom.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1290,7 +1290,7 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(CUSTOM_BUILD),)
+ifneq ($(RVOS_BUILD),)
 ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
